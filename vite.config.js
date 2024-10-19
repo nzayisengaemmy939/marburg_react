2151e11962888dd010c14626eeb19d0d6830fdd7
nzayisengaemmy939 <nzayisengaemmy2001@gmail.com>
2024-10-19 14:08:44 +0200

    my second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import imagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    imagemin({
+      gifsicle: {
+        optimizationLevel: 7,  // Optimizes GIFs
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,  // Optimizes PNGs
+      },
+      mozjpeg: {
+        quality: 70,  // Set JPEG quality (0-100)
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,  // Converts images to WebP with quality setting
+      },
+    }),
+  ],
+});
